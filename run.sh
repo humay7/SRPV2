cd9fdfa7a8fff68e04f29e5376d3432f971cc5ca
Humay Abbasli <70768897+humay7@users.noreply.github.com>
2024-10-01 15:21:17 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@
 #SBATCH --job-name=run_DA_exps
 #SBATCH --output=%x_%j.log
 #SBATCH --error=%x_%j.err
-#SBATCH --mail-user=mashalkar@uni-hildesheim.de
+#SBATCH --mail-user=abbasli@uni-hildesheim.de
 #SBATCH --partition=STUD
 #SBATCH --gres=gpu:1
 source activate GPU
-srun python evaluate.py
+srun python check_mnist.py
+
